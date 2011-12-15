4712332fcfd364fb4429678a0609372b2be82a3a
Jose Miguel Salcido Aguilar <eljose152@gmail.com>
2011-12-14 17:27:17 -0700

    Using classes and updating things

---
+++ functions.py
@@ -0,0 +1,12 @@
+'''
+functions.py
+Useful functions for the wordpress-cl program
+'''
+
+ERROR_MSG_BAD_USE = "That's not the way you should use this program"
+
+def exit():
+    print ERROR_MSG_BAD_USE
+
+if __name__ == "__main__":
+    exit()
--- main.py
+++ main.py
@@ -9,17 +9,10 @@ https://github.com/maxcutler/python-wordpress-xmlrpc
 #import wordpresslib
 from wordpress_xmlrpc import Client, WordPressPost
 from wordpress_xmlrpc.methods.posts import NewPost
+from config import Config
 import sys
+import functions
 
-ERROR_MSG_BAD_USE = "That's not the way you should use this program"
-_FILE = "config.wp"
-
-# Oh boy...
-_url      = ""
-_username = ""
-_pwd      = ""
-
-# 
 def main():
     if sys.argv[1:] == []:
         exit()
@@ -27,12 +20,11 @@ def main():
         execute()
 
 def execute():
-    config     = readConfig()
-    # TODO try except this ??
-    _url       = config["url"]
-    _username  = config["username"]
-    _pwd       = config["password"]
-    config     = {}
+    config     = Config()
+    config.readConfig()
+    _url = config.config["url"]
+    _username = config.config["username"]
+    _pwd = config.config["password"]
     title      = sys.argv[1]
     wp         = Client(_url, _username, _pwd)
     # Call new post
@@ -44,7 +36,7 @@ def preparePost(title):
     post.title = title
     try:
         file_name = sys.argv[2]
-    except Exception:
+    except IndexError:
         exit()
     post.description = readPost(file_name)
     return post
@@ -52,26 +44,10 @@ def preparePost(title):
 def readPost(file_name):
     try:
         fopen = open(file_name, 'r')
-	return fopen.read().replace("\n","")
-    except Exception:
+	#return fopen.read().replace("\n","")
+	return fopen.read()
+    except IOError:
         exit()
-
-def readConfig():
-    fopen = open(_FILE, 'r')
-    fopen.seek(0)
-    line = fopen.readline()
-    hashmap = {}
-    while line != '':
-        if line[0] == "#" or line[0] == "\n":
-            # Comments = #
-            line = fopen.readline()
-            continue
-        listaLine = line.split()
-        # Remove the '='
-        listaLine.pop(1)
-        hashmap[listaLine[0]] = listaLine[1]
-        line = fopen.readline()
-    return hashmap
 
 def exit():
     print ERROR_MSG_BAD_USE

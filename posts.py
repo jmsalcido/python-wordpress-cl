80debf9c8a9b3076d036fb7406898c6347d52c16
Jose Miguel Salcido Aguilar <eljose152@gmail.com>
2011-12-27 03:36:43 -0700

    posts.py: add some TODO's

--- posts.py
+++ posts.py
@@ -1,10 +1,16 @@
 from wordpress_xmlrpc import Client, WordPressPost
 from wordpress_xmlrpc.methods.posts import NewPost
-import functions
+
+# TODO
+# Class ManagePost
+#   Params: Post
+#   Methods: sendPost, readPost and preparePost
 
 class Post:
 	'''Post class for the wordpress-cl program'''
 	def readPost(self, file_name):
+		'''Read the post and return the parsed text'''
+		# TODO LOTS OF THINGS, must parse correctly the text and all, but its 4 o clock in the morning... c'mon.
 		try:
 			fopen = open(file_name, 'r')
 			txt = fopen.read()
@@ -13,16 +19,19 @@ class Post:
 			return ""
 
 	def sendPost(self):
+		'''Send post to the blog and return the post id'''
 		return self.wp_client.call(NewPost(self.post, True))
 		
 
 	def preparePost(self):
+		'''Prepare the post'''
 		post = WordPressPost()
 		post.title = self.title
 		post.description = self.readPost(self.file_name)
 		return post
 
 	def __init__(self, wp_client, title = "", file_name = ""):
+		'''Init class'''
 		self.title = title
 		self.file_name = file_name
 		self.wp_client = wp_client

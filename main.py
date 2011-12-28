b25210de35c8c95c28968ec3878d79127e40b3f7
Jose Miguel Salcido Aguilar <eljose152@gmail.com>
2011-12-28 02:43:16 -0700

    removing old code from main.py and adding TODO general list

--- main.py
+++ main.py
@@ -6,9 +6,10 @@ Main script for the wordpress-cl program
 Using python_wordpress_xmlrpc library:
 https://github.com/maxcutler/python-wordpress-xmlrpc
 '''
-#import wordpresslib
-from wordpress_xmlrpc import Client, WordPressPost
-from wordpress_xmlrpc.methods.posts import NewPost
+# TODO
+#
+
+from wordpress_xmlrpc import Client
 from config import Config
 from posts import Post, ManagePost
 import sys
@@ -32,27 +33,6 @@ def execute():
 	post       = Post(wp, title, file_name)
 	manager    = ManagePost(post)
 	manager.sendPost()
-
-#def preparePost(title):
-#	post = WordPressPost()
-#	post.title = title
-#	try:
-#		file_name = sys.argv[2]
-#	except IndexError:
-#		exit()
-#	post.description = readPost(file_name)
-#	return post
-
-#def readPost(file_name):
-#	try:
-#		fopen = open(file_name, 'r')
-#		#return fopen.read().replace("\n","")
-#		return fopen.read()
-#	except IOError:
-#		exit()
-
-#def exit():
-#	print ERROR_MSG_BAD_USE
 
 if __name__ == '__main__':
 	main()
